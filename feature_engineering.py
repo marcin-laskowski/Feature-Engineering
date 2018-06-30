f4aab3324fed92a099746e5ea847f070064274a6
mlaskowski17 <marcin.laskowski17@imperial.ac.uk>
2018-06-30 14:35:32 +0100

    improvment of the code

--- feature_engineering.py
+++ feature_engineering.py
@@ -7,6 +7,14 @@ Although there are now many efforts working to enable automated model selection 
 tuning, there has been a lack of automating work on the feature engineering aspect of the pipeline.
 This library seeks to close that gap and the general methodology has been proven effective in both
 machine learning competitions with the data science machine and business use cases.
+
+This is a powerful method which allows us to overcome the human limits of time and imagination to
+create many new features from multiple tables of data. Featuretools is built on the idea of deep
+feature synthesis, which means stacking multiple simple feature primitives - aggregations and
+transformations - to create new features. Feature engineering allows us to combine informatio
+across many tables into a single dataframe that we can then use for machine learning model training.
+Finally, the next step after creating all of these features is figuring out which ones are
+important.
 '''
 
 # Run this if featuretools is not already installed
@@ -161,3 +169,91 @@ r_payments = ft.Relationship(es['loans']['loan_id'], es['payments']['loan_id'])
 es = es.add_relationship(r_payments)
 
 print(es)
+
+
+# ==================================== FUTURE PRIMITIVES ===========================================
+
+# A feature primitive is an operation applied to data to create feature. These represent very simple
+# calculations that can be stacked on top of each other to create complex features.
+# Feature primitives fail into two categories:
+# 1. Aggregation: function that groups together child datapoints for each parent and then calculates
+# a statistic such as mean, min, max or standard deviation. An example is calcualting the maximum
+# loan amount for each client. An aggregation works across multiple tables using relationships
+# between tables.
+# 2. Transformation: an operation applied to one or more columns in a single table. An example
+# would be extracting the day from dates, or finiding the difference between two columns in one
+# table.
+
+# present list of primitives
+primitives = ft.list_primitives()
+pd.options.display.max_colwidth = 100
+primitives[primitives['type'] == 'aggregation'].head(10)
+primitives[primitives['type'] == 'transform'].head(10)
+
+
+# In featuretools it is also possible to create your own primitives.
+# the 'ft.dfs' function (which stands for: deep feature synthesis). In this function, we specify the
+# entityset to use; the target_entity, which is the dataframe we want to make the features for. The
+# agg_primitives which are the aggregation feature primitives; and the trans_primitives which are
+# transformation primitives to apply.
+
+# Create new features using specified primitives
+features, feature_names = ft.dfs(entityset=es, target_entity='clients',
+                                 agg_primitives=['mean', 'max', 'percent_true', 'last'],
+                                 trans_primitives=['years', 'month', 'subtract', 'divide'])
+
+pd.DataFrame(features['MONTH(joined)'].head())
+pd.DataFrame(features['MEAN(payments.payment_amount)'].head())
+features.head()
+
+
+# ================================= DEEP FEATURE SYNTHESIS =========================================
+
+# While feature primitives are useful by themselves, the main benefit of using featuretools arises
+# when we stack primitives to get deep features. The depth of a feature is simply the number of
+# primitives required to make a feature. So, a feature that relies on a single aggregation would be
+# a deep feature with a depth of 1, a feature that stacks two primitives would have a depth of 2
+# and so on. The idea itself is lot simpler than the name "deep feature synthesis" implies.
+# (more in the link: https://docs.featuretools.com/automated_feature_engineering/afe.html)
+
+
+# the MEAN(loans.loan_amount) feature has a depth of 1 because it is made by applying a single
+# aggregation primitive. This feature represents the average size of a client's previous loans.
+
+# show a feature with a depth of 1
+pd.DataFrame(features['MEAN(loans.loan_amount)'].head(10))
+
+# As well scroll through the features, we see a number of features with a depth of 2. For example,
+# the LAST(loans.(MEAN(payments.payment_amount))) has depth = 2 because it is made by stacking two
+# feature primitives, first an aggregation and then a transformation. This feature represents
+# the average payment amount for the last (most recent) loan for each client.
+
+# Show a feature with depth of 2
+pd.DataFrame(features['LAST(loans.MEAN(payments.payment_amount))'].head(10))
+
+# We can create features of arbitrary depth by stacking more primitives. However, it is important to
+# consider the fact that primitives with depth more than 2 become very convoluted to understand.
+
+
+# ============================== AUTOMATED DEEP FEATURE SYNTHESIS ==================================
+
+# In addition to manually specifying aggregation and transformation feature primitives, we can let
+# featuretools automatically generate many new features. We do this by making the same ft.dfs
+# function call, but without passing in any primitives. We just set the max_depth parameter and
+# featuretools will automatically try many all combinations of feature primitives to the ordered
+# depth
+
+# perform deep feature synthesis without specifying primitives
+features, feature_name = ft.dfs(entityset=es, target_entity='clients', max_depth=2)
+features.iloc[:, 4:].head()
+
+# Deep feature synthesis has created 90 new features out of the existing data! While we could have
+# created all of these manually, I am glad to not have to write all that code by hand. The primary
+# benefit of featuretools is that it creates features without any subjective human biases. Even a
+# human with considerable domain knowledge will be limited by their imagination when making new
+# features (not to mention time). Automated feature engineering is not limited by these factors
+# (instead it's limited by computation time) and provides a good starting point for feature
+# creation. This process likely will not remove the human contribution to feature engineering
+# completely because a human can still use domain knowledge and machine learning expertise to
+# select the most important features or build new features from those suggested by automated deep
+# feature synthesis.
